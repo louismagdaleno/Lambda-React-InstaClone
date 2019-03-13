211b24cf68444e5193f7372415c2bde8e7465925
Louis Magdaleno <buildthatapp@gmail.com>
2019-03-13 09:47:30 -0700

    Added SearchBar component

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,3 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './_SearchBar.scss';
+
+// image imports
+import logo from './instagram.png';
+import icon from './logo.png';
+import heart from './heart.png';
+import post from './post.png';
+import user from './user.png';
+
+const SearchBar = props => {
+    return (
+        <header className="searchBar">
+            <div className="searchBar_logos">
+                <img src={icon} alt="Instagram Logo" />
+                |
+                <img src={logo} alt="Instagram Logo" />
+            </div>
+
+            <input type="text" placeholder="🔍 Search" />
+
+            <div className="searchBar_icons">
+                <img src={post} alt="Write a post" />
+                <img src={heart} alt="View photos you've liked" />
+                <img src={user} alt="View your profile" />
+            </div>
+        </header>
+    );
+}
+
+SearchBar.propTypes = {
+    props: PropTypes.object
+}
+
+export default SearchBar;
