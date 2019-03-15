e7b1655ca1e637efb0e798140b05f1a12cfcec76
Louis Magdaleno <buildthatapp@gmail.com>
2019-03-15 10:31:19 -0700

    Refactored CommentSection into a class component

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './_CommentSection.scss';
 import Comment from '../Comment/Comment';
 import PropTypes from 'prop-types';
 
-const CommentSection = (props) => {
-    return (
-        <div className="comments">
-            {props.comments.map((comment, index) => <Comment comment={comment} key={index} />)}
-        </div>
-    );
+class CommentSection extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            comments : props.comments
+        }
+    }
+    render () {
+        return (
+            <div className="comments">
+                {this.state.comments.map((comment, index) => <Comment comment={comment} key={index} />)}
+            </div>
+        );
+    }
+    
 }
 
 CommentSection.propTypes = {
