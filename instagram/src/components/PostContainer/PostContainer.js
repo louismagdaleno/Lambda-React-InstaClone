144134cfc817461f2a9e9c6b9dfde1182647f9d6
Louis Magdaleno <buildthatapp@gmail.com>
2019-03-15 11:16:52 -0700

    Added new feature to like post

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './_PostContainer.scss';
 import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
 import heart from './heart.png';
+import redheart from './redheart.png';
 import comment from './comment.png';
 
-const PostContainer = (props) => {
-    return (
-        <div className="postContainer">
-            <header>
-                <img src={props.data.thumbnailUrl} />
-                <h3>{props.data.username} </h3>
-            </header>
-            <img src={props.data.imageUrl} />
-            <div className="actions">
-                <img src={heart} />
-                <img src={comment} />
+class PostContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            likes : props.data.likes,
+            liked: false
+        }
+    }
+
+    likePost = () => {
+        this.setState(prevState => ({
+            liked: !prevState.liked,
+            likes: !prevState.liked ? prevState.likes + 1 : prevState.likes - 1
+        }))
+    }
+
+    render() {
+        return (
+            <div className="postContainer">
+                <header>
+                    <img src={this.props.data.thumbnailUrl} />
+                    <h3>{this.props.data.username} </h3>
+                </header>
+                <img src={this.props.data.imageUrl} />
+                <div className="actions">
+                    <img src={this.state.liked ? redheart : heart} onClick={this.likePost} />
+                    <img src={comment} />
+                </div>
+                <div className="likes">
+                    <p>{this.state.likes} likes </p>
+                </div>
+                <CommentSection className="commentSection" comments={this.props.data.comments} />
+                
             </div>
-            <div className="likes">
-                <p>{props.data.likes} likes </p>
-            </div>
-            <CommentSection className="commentSection" comments={props.data.comments} />
-            
-        </div>
-    );
+        );
+    }
+    
 }
 
 PostContainer.propTypes = {
